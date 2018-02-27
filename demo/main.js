f8b6628fe47c8c27f5b416728cbe90e6274414e8
Shannon Poole <shannon.m.poole@gmail.com>
2018-02-26 22:33:13 -0500

    making progress on renderer

--- demo/main.js
+++ demo/main.js
@@ -2,6 +2,11 @@ import { Renderer, WebGLTF } from '../lib/webgltf.js';
 
 (async () => {
   const renderer = new Renderer('#main');
-  const model = await WebGLTF.fetch('https://rawgit.com/KhronosGroup/glTF-Sample-Models/master/2.0/AnimatedCube/glTF/AnimatedCube.gltf');
-  model.init(renderer);
+  const model = await WebGLTF.load('https://cdn.rawgit.com/KhronosGroup/glTF-Sample-Models/8416be1c/2.0/DamagedHelmet/glTF/DamagedHelmet.gltf');
+  const camera = model.createCamera();
+
+  requestAnimationFrame(function tick() {
+    requestAnimationFrame(tick);
+    renderer.render(model.scene, camera);
+  });
 })();
--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,56 @@
-import uglify from 'rollup-plugin-uglify';
+import resolve  from 'rollup-plugin-node-resolve';
+import virtual  from 'rollup-plugin-virtual';
+import uglify   from 'rollup-plugin-uglify';
 
-export default [{
-  input: 'lib/webgltf.js',
-  output: [{
-    file: 'dist/webgltf.js',
-    format: 'es',
-    sourcemap: true,
+export default [
+  {
+    input: '__virtual__',
+    plugins: [
+      virtual({
+        __virtual__: 'export * from \'gl-matrix\';',
+      }),
+      resolve(),
+    ],
+    output: {
+      file: 'vendor/gl-matrix.js',
+      format: 'es',
+      sourcemap: true,
+    },
+  },
+  {
+    input: 'lib/webgltf.js',
+    output: [
+      {
+        file: 'dist/webgltf.js',
+        format: 'es',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/webgltf.umd.js',
+        format: 'umd',
+        name: 'gltf',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
   }, {
-    file: 'dist/webgltf.umd.js',
-    format: 'umd',
-    name: 'gltf',
-    exports: 'named',
-    sourcemap: true,
-  }],
-}, {
-  input: 'lib/webgltf.js',
-  plugins: [
-    uglify(),
-  ],
-  output: [{
-    file: 'dist/webgltf.min.js',
-    format: 'es',
-    sourcemap: true,
-  }, {
-    file: 'dist/webgltf.umd.min.js',
-    format: 'umd',
-    name: 'gltf',
-    exports: 'named',
-    sourcemap: true,
-  }],
-}];
+    input: 'lib/webgltf.js',
+    plugins: [
+      uglify(),
+    ],
+    output: [
+      {
+        file: 'dist/webgltf.min.js',
+        format: 'es',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/webgltf.umd.min.js',
+        format: 'umd',
+        name: 'gltf',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+  },
+];
