2c2c8af178494de0b196689d0c1ccc39c8d7d4b0
Shannon Poole <shannon.m.poole@gmail.com>
2019-11-23 09:25:49 -0500

    extension work in progress

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,27 @@
+import fetch      from 'node-fetch';
 import resolve    from 'rollup-plugin-node-resolve';
 import virtual    from 'rollup-plugin-virtual';
-import { terser } from 'rollup-plugin-terser';
+
+function dracobase64() {
+  return {
+    name: 'dracobase64',
+    resolveId(source) {
+      try {
+        const url = new URL(source);
+        return url.href;
+      } catch(e) {
+        return null;
+      }
+    },
+    async load(id) {
+      if (/^https/.test(id)) {
+        const buffer = await fetch(id).then(res => res.arrayBuffer());
+        return `export default '${Buffer.from(buffer).toString('base64')}';`
+      }
+      return null;
+    },
+  }
+}
 
 export default [
   {
@@ -18,6 +39,24 @@ export default [
     },
   },
   {
+    input: '__virtual__',
+    plugins: [
+      virtual({
+        __virtual__: `
+          import wrapper from 'https://raw.githubusercontent.com/google/draco/8833cf878e6fd43c5a3fd/javascript/draco_wasm_wrapper_gltf.js';
+          import wasm from 'https://raw.githubusercontent.com/google/draco/8833cf878e6fd43c5a3fd/javascript/draco_decoder_gltf.wasm';
+          export { wrapper, wasm };
+        `,
+      }),
+      dracobase64(),
+    ],
+    output: {
+      file: 'vendor/draco-decoder.js',
+      format: 'es',
+      sourcemap: true,
+    },
+  },
+  {
     input: 'lib/webgltf.js',
     output: [
       {
@@ -25,32 +64,15 @@ export default [
         format: 'es',
         sourcemap: true,
       },
-      {
-        file: 'dist/webgltf.umd.js',
-        format: 'umd',
-        name: 'WebGLTF',
-        exports: 'named',
-        sourcemap: true,
-      },
     ],
-  }, {
-    input: 'lib/webgltf.js',
-    plugins: [
-      terser(),
-    ],
-    output: [
-      {
-        file: 'dist/webgltf.min.js',
-        format: 'es',
-        sourcemap: true,
-      },
-      {
-        file: 'dist/webgltf.umd.min.js',
-        format: 'umd',
-        name: 'WebGLTF',
-        exports: 'named',
-        sourcemap: true,
-      },
-    ],
+  },
+  {
+    input: 'lib/extensions/KHR_draco_mesh_compression.js',
+    output: {
+      file: 'dist/extensions/KHR_draco_mesh_compression.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    external: ['../webgltf.js'],
   },
 ];
