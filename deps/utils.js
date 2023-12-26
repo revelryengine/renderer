29a4556e20b853f2c49fc7567ff4b4d132b052d1
Shannon Poole <shannon.m.poole@gmail.com>
2023-12-26 18:46:45 -0500

    treat alpha mask as opaque

--- deps/utils.js
+++ deps/utils.js
@@ -1,8 +1,8 @@
-export { groupBy           } from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.0-alpha/lib/array-grouping.js';
-export { merge             } from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.0-alpha/lib/merge.js';
-export { CanvasAutoResizer } from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.0-alpha/lib/canvas-auto-resizer.js';
-export { WeakCache         } from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.0-alpha/lib/weak-cache.js';
-export { PRNG              } from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.0-alpha/lib/prng.js';
+export { groupBy           } from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.1-alpha/lib/array-grouping.js';
+export { merge             } from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.1-alpha/lib/merge.js';
+export { CanvasAutoResizer } from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.1-alpha/lib/canvas-auto-resizer.js';
+export { WeakCache         } from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.1-alpha/lib/weak-cache.js';
+export { PRNG              } from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.1-alpha/lib/prng.js';
 
-export * from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.0-alpha/lib/buffers.js';
-export * from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.0-alpha/lib/math.js';
+export * from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.1-alpha/lib/buffers.js';
+export * from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.1-alpha/lib/math.js';
